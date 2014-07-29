6b3fe603c32efad3fdf2bf987cd3c7a4d1865603
Saran Vellanki <sarankumarvellanki@gmail.com>
2014-07-29 16:50:20 -0700

    Added RequestMethod.GET to SearchController

--- src/main/java/com/searchforit/controller/SearchController.java
+++ src/main/java/com/searchforit/controller/SearchController.java
@@ -2,19 +2,21 @@ package com.searchforit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SearchController {
-	String displayText = "Test";
+	String displayText = "Search the inventory for anything you like!";
 
-	@RequestMapping("/search")
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public ModelAndView search() {
 
 		/*
 		 * return a ModelAndView object taking the viewName, modelName and model
 		 * Object
 		 */
+		System.out.println("Logging the Search Controller entry!");
 		return new ModelAndView("search", "displayText", displayText);
 	}
 }
--- src/main/webapp/WEB-INF/views/search.jsp
+++ src/main/webapp/WEB-INF/views/search.jsp
@@ -4,9 +4,17 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Search for it!</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<h1>${displayText}</h1>
+	<br>
+	<input type="text" class="form-control"
+		placeholder="Search for something..">
+	<br>
+	<button type="button" class="btn btn-success" disabled="disabled">Submit</button>
 </body>
 </html>
