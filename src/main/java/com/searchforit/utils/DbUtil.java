030febcddcb154725d9e7e4474bdcac87b36a8fc
Saran Vellanki <sarankumarvellanki@gmail.com>
2014-08-02 13:22:49 -0700

    Documents need to be added for the collection to be shown in the database. Done.

--- src/main/java/com/searchforit/utils/DbUtil.java
+++ src/main/java/com/searchforit/utils/DbUtil.java
@@ -3,41 +3,42 @@ package com.searchforit.utils;
 import java.net.UnknownHostException;
 import java.util.Set;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 public class DbUtil {
-	public static void setUp() throws UnknownHostException{
+	public static void setUp() throws UnknownHostException {
+	   
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
-//		DB db = mongoClient.getDB("inventory");
-//		db.getCollection("author");
-//		db.getCollection("bookName");
-		
+		DB books = mongoClient.getDB("books");
 
-		List<String> dbList = mongoClient.getDatabaseNames();
+		DBCollection catalog = books.getCollection("catalog");
+
+		DBObject book = new BasicDBObject();
+
+		book.put("author", "Gambardella, Matthew");
+		book.put("title", "XML Developer's Guide");
+		book.put("genre", "Computer");
+		book.put("price", "44.95");
+		book.put("publish_date", "2000-10-01");
+		book.put("description",
+				"An in-depth look at creating applications with XML.");
 		
-		for(String dbName: dbList) {
-			System.out.println(dbName);
-		}
+		catalog.insert(book);
 		
-		
-		
-		DB db = mongoClient.getDB("testdb");
-		DBCollection table = db.getCollection("user");
-		
-		Set<String> tables = db.getCollectionNames();
-	 
-		for(String coll : tables){
+		Set<String> catalogTables = books.getCollectionNames();
+
+		for (String coll : catalogTables) {
 			System.out.println(coll);
 		}
 	}
-	
-	
-	public static void main(String [] args) throws UnknownHostException {
+
+	public static void main(String[] args) throws UnknownHostException {
 		System.out.println("hello");
 		setUp();
 	}
-	
-}
 
+}
