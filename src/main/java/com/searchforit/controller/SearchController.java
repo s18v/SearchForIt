5176401ca71faeb74b8add6c5d72305ae1b967ba
Saran Vellanki <sarankumarvellanki@gmail.com>
2014-08-04 17:41:01 -0700

    added @RequestParam to the SearchController

--- src/main/java/com/searchforit/controller/SearchController.java
+++ src/main/java/com/searchforit/controller/SearchController.java
@@ -3,20 +3,31 @@ package com.searchforit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SearchController {
 	String displayText = "Search the inventory for anything you like!";
 
-	@RequestMapping(value = "/search", method = RequestMethod.GET)
-	public ModelAndView search() {
+	// @RequestMapping(value = "/search", method = RequestMethod.GET)
+	// public ModelAndView search() {
+	//
+	// /*
+	// * return a ModelAndView object taking the viewName, modelName and model
+	// * Object
+	// */
+	// System.out.println("Logging the Search Controller entry!");
+	// return new ModelAndView("search", "displayText", displayText);
+	// }
 
-		/*
-		 * return a ModelAndView object taking the viewName, modelName and model
-		 * Object
-		 */
-		System.out.println("Logging the Search Controller entry!");
-		return new ModelAndView("search", "displayText", displayText);
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public ModelAndView handleSearch(
+			@RequestParam(value = "searchQuery", required = false) String searchQuery) {
+
+		System.out.println("trying to get the search query");
+
+		return new ModelAndView("search", "searchQuery", searchQuery);
 	}
+
 }
